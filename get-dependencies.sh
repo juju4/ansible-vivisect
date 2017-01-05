64fc7b36941c8027d25c62ed5dbb891ed6293767
juju4 <juju4@users.noreply.github.com>
2017-01-05 02:47:56 -0500

    sync repositories - add Jenkinsfile, galaxy naming

---
+++ get-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+rolesdir=$(dirname $0)/..
+
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.vivisect ] && ln -s ansible-vivisect $rolesdir/juju4.vivisect
+[ ! -e $rolesdir/juju4.vivisect ] && cp -R $rolesdir/ansible-vivisect $rolesdir/juju4.vivisect
+
+## don't stop build on this script return code
+true
+
