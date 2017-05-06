c50f4c48eac4ac8d7cdf6c2c5836522955874110
juju4 <juju4@users.noreply.github.com>
2017-05-06 10:35:03 -0400

    add epel for python3 and centos7

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,7 +5,7 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.vivisect ] && ln -s ansible-vivisect $rolesdir/juju4.vivisect
 [ ! -e $rolesdir/juju4.vivisect ] && cp -R $rolesdir/ansible-vivisect $rolesdir/juju4.vivisect
